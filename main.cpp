09a2a2b5f9f4cb841350a99f78122c624447bee4
hayatetoon@icloud.com <hayatetoon@icloud.com>
2023-05-12 11:02:36 +0900

    01_02_確認課題

--- main.cpp
+++ main.cpp
@@ -23,6 +23,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	win->CreateGameWindow("GC2B_09_ミヤウチ_ハヤテ_AL3");
 
 
+
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
 	dxCommon->Initialize(win);
